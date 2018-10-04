d3520d268e8b03131dfc22023ad5aa7f0ecc9c82
npelletier628 <npelletier2000@gmail.com>
2018-10-04 15:17:54 -0400

    HI

---
+++ app/src/main/java/com/example/brandonly/helloworld/np7437.java
@@ -0,0 +1,13 @@
+package com.example.brandonly.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class np7437 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_np7437);
+    }
+}
